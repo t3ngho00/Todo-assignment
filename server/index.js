59c6d467662bf803b100c5f5740b04e6d5c2209e
Nguyen Hoang Thach <t3ngho00@students.oamk.fi>
2024-03-24 14:55:19 +0200

    Implement environment variables and separate database module

--- server/index.js
+++ server/index.js
@@ -43,6 +43,20 @@ app.post("/", async (req, res) => {
     }
 });
 
+app.delete("/delete/:id", async (req, res) => {
+    const id = parseInt(req.params.id);
+    const client = await pool.connect();
+    try {
+      await client.query('DELETE FROM task WHERE id = $1', [id]);
+      res.status(200).send(`Task deleted with ID: ${id}`);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Internal Server Error" });
+    } finally {
+      client.release();
+    }
+  });
+
 app.listen(3001, () => {
     console.log("Server started on port 3001");
 });
