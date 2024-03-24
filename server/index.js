10add54893f3084db237cb2f13bf76973f5ccfc5
Nguyen Hoang Thach <t3ngho00@students.oamk.fi>
2024-03-24 14:00:09 +0200

    Add backend with Node.js, Express and PostgreSQL

---
+++ server/index.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const cors = require("cors");
+const { Pool } = require("pg");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const port = 3001;
+
+const pool = new Pool({
+    user: "postgres",
+    host: "localhost",
+    database: "todo",
+    password: "12345",
+    port: 5432,
+  });
+  
+  const getTasks = async () => {
+    const client = await pool.connect();
+    try {
+      const res = await client.query("SELECT * FROM task");
+      return res.rows;
+    } catch (err) {
+      console.error(err);
+      return null;
+    } finally {
+      client.release();
+    }
+  };
+
+  
+  app.get("/", async (req, res) => {
+    try {
+      const tasks = await getTasks();
+      res.status(200).json(tasks);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  });
+app.listen(port);
+
+
+app.post("/", async (req, res) => {
+    const { description } = req.body;
+    const client = await pool.connect();
+    try {
+      const result = await client.query(
+        "INSERT INTO task (description) VALUES ($1) RETURNING *",
+        [description]
+      );
+      res.status(200).json(result.rows[0]);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: "Internal Server Error" });
+    } finally {
+      client.release();
+    }
+  });
