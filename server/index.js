85b6445b349c8e126b649411d7f76e4fcb41f87c
Nguyen Hoang Thach <t3ngho00@students.oamk.fi>
2024-03-24 14:47:55 +0200

    Implement environment variables and separate database module

--- server/index.js
+++ server/index.js
@@ -1,73 +1,48 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
-const { Pool } = require("pg");
+const query = require("./helpers/db");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "todo",
-  password: "12345",
-  port: 5432,
-});
-
 const getTasks = async () => {
-  const client = await pool.connect();
-  try {
-    const res = await client.query("SELECT * FROM task");
-    return res.rows;
-  } catch (err) {
-    console.error(err);
-    return null;
-  } finally {
-    client.release();
-  }
+    try {
+        const res = await query("SELECT * FROM task");
+        return res.rows;
+    } catch (err) {
+        console.error(err);
+        return null;
+    }
 };
 
 app.get("/", async (req, res) => {
-  try {
-    const tasks = await getTasks();
-    res.status(200).json(tasks);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
+    try {
+        const tasks = await getTasks();
+        res.status(200).json(tasks);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
 });
 
 app.post("/", async (req, res) => {
-  const { description } = req.body;
-  const client = await pool.connect();
-  try {
-    const result = await client.query(
-      "INSERT INTO task (description) VALUES ($1) RETURNING *",
-      [description]
-    );
-    res.status(200).json(result.rows[0]);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: "Internal Server Error" });
-  } finally {
-    client.release();
-  }
+    const { description } = req.body;
+    try {
+        const result = await query(
+            "INSERT INTO task (description) VALUES ($1) RETURNING *",
+            [description]
+        );
+        res.status(200).json(result.rows[0]);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
 });
 
-app.delete("/delete/:id", async (req, res) => {
-  const id = parseInt(req.params.id);
-  const client = await pool.connect();
-  try {
-    await client.query('DELETE FROM task WHERE id = $1', [id]);
-    res.status(200).send(`Task deleted with ID: ${id}`);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Internal Server Error" });
-  } finally {
-    client.release();
-  }
-});
 app.listen(3001, () => {
-  console.log("Server started on port 3001");
+    console.log("Server started on port 3001");
 });
